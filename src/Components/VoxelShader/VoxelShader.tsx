5500815c923f00c073031665665eb3b8f76527d9
kndlt <kndlt22@gmail.com>
2019-01-01 19:13:23 -0800

    glsl noise

--- src/Components/VoxelShader/VoxelShader.tsx
+++ src/Components/VoxelShader/VoxelShader.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Shaders, Node, GLSL } from "gl-react";
-import { randomFunction } from "../../Shaders/randomFunction";
-import MyGl from '../../Shaders/MyGl.glsl';
+import sampleShader from '../../Shaders/sampleShader.glsl';
 
 interface VoxelShaderProps {
   eye: number[];
@@ -11,23 +10,7 @@ interface VoxelShaderProps {
 
 const shaders = Shaders.create({
   vt01: {
-    frag: GLSL`
-      precision highp float;
-      varying vec2 uv;
-      uniform mat4 matrixWorldInverse;
-      uniform mat4 projectionMatrixInverse;
-      uniform vec3 eye;
-      ${randomFunction}
-      void main() {
-        // // TODO Optimize by pre-computing matrices or initial rays itself
-        // vec4 q = matrixWorldInverse * projectionMatrixInverse * vec4((uv - 0.5) * 2.0, 0.0, 1.0);
-        // vec3 initialRay = normalize(q.xyz / q.w - eye);
-        // gl_FragColor = projectionMatrixInverse * vec4(initialRay * 255.0, 1.0);
-
-        float rand = random(vec3(12.9898, 78.233, 151.7182), uv.x);
-        gl_FragColor = vec4(rand, rand, rand, 1.0);
-      }
-    `
+    frag: GLSL`${sampleShader}`
   }
 });
 
@@ -36,8 +19,6 @@ export default class VoxelShader extends React.Component<VoxelShaderProps> {
   render() {
 
     const { eye, matrixWorldInverse, projectionMatrixInverse } = this.props;
-
-    console.log(MyGl);
 
     // console.log(projectionMatrixInverse.multiplyVector4(new Vector4(1,1,0,1)));
     return (
---
+++ src/Shaders/sampleShader.glsl
@@ -0,0 +1,23 @@
+// #pragma glslify: random = require(./Functions/random)
+
+precision highp float;
+
+varying vec2 uv;
+uniform mat4 matrixWorldInverse;
+uniform mat4 projectionMatrixInverse;
+uniform vec3 eye;
+
+#pragma glslify: random = require('glsl-random')
+
+void main() {
+  // // TODO Optimize by pre-computing matrices or initial rays itself
+  // vec4 q = matrixWorldInverse * projectionMatrixInverse * vec4((uv - 0.5) * 2.0, 0.0, 1.0);
+  // vec3 initialRay = normalize(q.xyz / q.w - eye);
+  // gl_FragColor = projectionMatrixInverse * vec4(initialRay * 255.0, 1.0);
+
+  // float rand = random(vec3(12.9898, 78.233, 151.7182), uv.x);
+  // gl_FragColor = vec4(rand, rand, rand, 1.0);
+
+  float n = random(uv);
+  gl_FragColor = vec4(vec3(n), 1.0);
+}
